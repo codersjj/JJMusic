f380545ec212670ace6d269210d8d1d610895408
Shane678 <shanewestlife@outlook.com>
2022-01-08 17:15:02 +0800

    style(music): scroll-view 的宽度设置为屏幕宽度（方案二）
    通过 100vw 直接将宽度设置视口（屏幕）的宽度

--- components/song-menu-area/index.js
+++ components/song-menu-area/index.js
@@ -1,6 +1,4 @@
 // components/song-menu-area/index.js
-const app = getApp()
-
 Component({
   /**
    * 组件的属性列表
@@ -13,7 +11,7 @@ Component({
    * 组件的初始数据
    */
   data: {
-    screenWidth: app.globalData.screenWidth
+
   },
 
   /**
