d74bf57398e462215a9951879d95828c4b05ae50
Shane678 <shanewestlife@outlook.com>
2022-01-08 16:48:38 +0800

    feat(music): add popular song menu list
    音乐页面 - 歌单区域的布局细节
    scroll-view 的宽度设置为屏幕宽度（方案一）

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 // app.js
 App({
+  onLaunch() {
+    // 在应用程序启动时获取系统信息
+    const systemInfo = wx.getSystemInfoSync()
+    // 屏幕宽度、高度不会变，没有必要去使用响应式的全局状态管理 hy-event-store，直接存在 app 的 globalData 中即可
+    this.globalData.screenWidth = systemInfo.screenWidth
+    this.globalData.screenHeight = systemInfo.screenHeight
+  },
+  globalData: {
+    screenWidth: 0,
+    screenHeight: 0
+  }
 })
