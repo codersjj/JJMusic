4078df5169a82d35906f1d320ae4adaee82698fe
Shane678 <shanewestlife@outlook.com>
2022-01-09 13:49:11 +0800

    perf(music): switch 改写为对象映射
    音乐页面 - 排行榜数据的请求和处理

--- store/ranking-store.js
+++ store/ranking-store.js
@@ -2,6 +2,13 @@ import { HYEventStore } from 'hy-event-store'
 
 import { getRankings } from "../service/api_music";
 
+const rankingMap = {
+  0: 'newRanking',
+  1: 'hotRanking',
+  2: 'originRanking',
+  3: 'upRanking'
+}
+
 const rankingStore = new HYEventStore({
   state: {
     newRanking: {}, // 新歌榜
@@ -14,20 +21,8 @@ const rankingStore = new HYEventStore({
       // 0: 新歌榜 1: 热歌榜 2: 原创榜 3: 飙升榜
       for (let i = 0; i < 4; i++) {
         getRankings(i).then(res => {
-          switch (i) {
-            case 0: 
-              ctx.newRanking = res.playlist
-              break;
-            case 1: 
-              ctx.hotRanking = res.playlist
-              break;
-            case 2: 
-              ctx.originRanking = res.playlist
-              break;
-            case 3: 
-              ctx.upRanking = res.playlist
-              break;
-          }
+          const rankingName = rankingMap[i]
+          ctx[rankingName] = res.playlist
         })
       }
     }
