2727e6287fceb19405d177415d883d0a49c64dd0
Shane678 <shanewestlife@outlook.com>
2022-01-16 21:43:39 +0800

    feat(search): 使用富文本高亮搜索内容

--- pages/search-detail/index.js
+++ pages/search-detail/index.js
@@ -12,6 +12,7 @@ Page({
   data: {
     hotKeywords: [],
     suggestSongs: [],
+    suggestSongsNodes: [],
     searchValue: '',
     getSearchResponse: false
   },
@@ -40,17 +41,71 @@ Page({
     this.setData({searchValue})
     // 3. 判断关键字为空字符串时的处理逻辑
     if (!searchValue.length) {
-      this.setData({ suggestSongs: [] })
+      this.setData({ suggestSongs: [], suggestSongsNodes: [] })
       debounceGetSearchSuggest.cancel()
       return
     }
     // 4. 根据输入的内容进行搜索
     debounceGetSearchSuggest(searchValue).then(res => {
+      // 1. 获取建议的歌曲
       let suggestSongs = res.result.allMatch
       if (!suggestSongs) {
         suggestSongs = []
       }
       this.setData({ suggestSongs, getSearchResponse: true })
+
+      // 2. 转成 node 节点
+      const suggestKeywords = suggestSongs.map(item => item.keyword)
+      const suggestSongsNodes = []
+      for (const keyword of suggestKeywords) {
+        const nodes = []
+        if (keyword.toUpperCase().startsWith(searchValue.toUpperCase())) {
+          const key1 = keyword.slice(0, searchValue.length)
+          const key2 = keyword.slice(searchValue.length)
+          const node1 = {
+            name: 'span',
+            attrs: {
+              style: 'color: #26ce8a;'
+            },
+            children: [
+              {
+                type: 'text',
+                text: key1
+              }
+            ]
+          }
+          nodes.push(node1)
+          const node2 = {
+            name: 'span',
+            attrs: {
+              style: 'color: #000;'
+            },
+            children: [
+              {
+                type: 'text',
+                text: key2
+              }
+            ]
+          }
+          nodes.push(node2)
+        } else {
+          const node = {
+            name: 'span',
+            attrs: {
+              style: 'color: #000;'
+            },
+            children: [
+              {
+                type: 'text',
+                text: keyword
+              }
+            ]
+          }
+          nodes.push(node)
+        }
+        suggestSongsNodes.push(nodes)
+      }
+      this.setData({ suggestSongsNodes })
     })
   }
 
